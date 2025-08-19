e5ecd319e846fca2b0f1fe69682e8b055b598f50
Anne Brown <anne@browndomain.com>
2025-08-19 12:42:18 -0400

    CI errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,11 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-    {
-        rules: {
-        'indent': ['error', 4],
+    // {
+        // rules: {
+        // 'indent': ['error', 4],
         // 'quotes': ['error', 'single'],
         // 'semi': ['error', 'always'],
-        },
-    },
+    //     },
+    // },
 )
