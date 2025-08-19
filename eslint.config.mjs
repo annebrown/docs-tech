7019c9b1bfa76d85331365418e6c30f9a8793ff9
Anne Brown <anne@browndomain.com>
2025-08-19 14:06:03 -0400

    Moved .editorconfig indent_style b4 indent_size

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,5 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-    // {
-        // rules: {
-        // 'indent': ['error', 4],
-        // 'quotes': ['error', 'single'],
-        // 'semi': ['error', 'always'],
-    //     },
-    // },
+
 )
